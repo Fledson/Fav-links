35f9211ae0422f1f5278607a68e4589ad37191c4
Fledson henrique <fledsonhenrique@gmail.com>
2021-05-04 16:18:49 -0300

    inserindo tratamento de rotas

--- api/index.js
+++ api/index.js
@@ -1,9 +1,30 @@
-const http = require("http");
-const data = require("./urls.json");
-const URL = require("url")
+const http = require('http')
+const URL = require('url')
+const fs = require('fs')
+const path = require('path')
+
+const data = require('./urls.json')
 
 
 http.createServer((req, res) => {
- console.log(URL.parse(req.url, true))
+    const {name, url, del} = URL.parse(req.url, true).query;
 
+    if(!name || !url)
+        return res.end(JSON.stringify(data))
+    
+    if (del) {
+        data.urls = data.urls.filter(item => String(item.url) !== String(url))
+
+        fs.writeFile(
+            path.join(__dirname, "urls.json"), 
+            JSON.stringify(data, null, 2),
+            err => {
+                if(err) throw err
+    
+                res.end(JSON.stringify({message: "ok"}));
+            }
+        )
+    }
+
+    return res.end("create")
 }).listen(3333, () => console.log("API is running"));
